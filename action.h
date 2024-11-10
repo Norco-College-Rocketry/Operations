b761fa05cc35b396491f915e376026ffa44c2add
ahthae <122731557+ahthae@users.noreply.github.com>
2024-11-10 01:35:01 -0800

    Expose actions to QML

--- action.h
+++ action.h
@@ -7,31 +7,46 @@
 #include "commandservice.h"
 
 namespace NCR {
-
-class Action : public QObject {
-  Q_OBJECT
+class Action {
   QML_INTERFACE
+
 public:
   Action() = default;
+
   virtual ~Action() = default;
 
-  Q_INVOKABLE virtual void execute() { }
+  Q_INVOKABLE virtual void execute() = 0;
 };
+}
+  Q_DECLARE_INTERFACE(NCR::Action, "Action")
 
-class CommandAction : public Action {
+namespace NCR {
+class CommandAction : public QObject, public Action {
   Q_OBJECT
-  Q_PROPERTY(Command command READ command WRITE command NOTIFY commandChanged)
+  Q_PROPERTY(CommandService* service READ service WRITE service NOTIFY serviceChanged)
   QML_ELEMENT
+  QML_IMPLEMENTS_INTERFACES(NCR::Action)
 public:
   CommandAction() =default;
   CommandAction(const Command& command, CommandService* service) : command_(command), service_(service) { }
 
-  void execute() override {
+  Q_INVOKABLE void execute() override {
     service_->send_command(command_);
   }
 
+  Q_INVOKABLE void set_command(const QString& command) { command_.command(command.toStdString()); }
+  Q_INVOKABLE void set_parameter(const QString& name, const QString& value) {
+    command_.set_parameter(name.toStdString(), value.toStdString());
+  }
+  Q_INVOKABLE void remove_parameter(const QString& name) { command_.remove_parameter(name.toStdString()); }
+
+  CommandService* service() { return service_; }
   Command& command() { return command_; }
 
+  void service(CommandService* service) {
+    service_ = service;
+    emit serviceChanged();
+  }
   void command(const Command& command) {
     command_ = command;
     emit commandChanged();
@@ -39,11 +54,12 @@ public:
 
 signals:
   void commandChanged();
+  void serviceChanged();
 
 private:
   CommandService* service_;
   Command command_;
 };
-
 }
+
 #endif // ACTION_H
