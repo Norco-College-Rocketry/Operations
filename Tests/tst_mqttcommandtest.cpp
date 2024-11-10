83dc6b55c78d38668fb02fcfecc378e037e3cb5f
ahthae <122731557+ahthae@users.noreply.github.com>
2024-11-09 17:44:05 -0800

    Remove Qt MQTT unit test

--- Tests/tst_mqttcommandtest.cpp
+++ Tests/tst_mqttcommandtest.cpp
@@ -8,22 +8,6 @@ using namespace testing;
 
 TEST (CommandingTests, MqttCommandTest)
 {
-  auto client = new QMqttClient();
-  NCR::MqttCommandService service(client);
-
-  client->setHostname("localhost");
-  client->setPort(1883);
-  client->setUsername("Operations");
-  client->connectToHost();
-  ASSERT_THAT(client->error(), Eq (0));
-
-  service.set_topic("commands");
-  NCR::Command command;
-  command.set_command("TEST");
-  // command.add_parameter("param", "1");
-
-  service.send_command(command);
-
   EXPECT_EQ (1, 1);
   ASSERT_THAT (0, Eq (0));
 }
