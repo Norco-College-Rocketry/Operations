8082795b2a41ca4227ea7a9717ed45a8ddb79312
ahthae <122731557+ahthae@users.noreply.github.com>
2024-11-10 01:35:37 -0800

    Pass command strings by const reference

--- command.h
+++ command.h
@@ -13,19 +13,19 @@ public:
   Command(std::string command) : command_(command) { }
   virtual ~Command() =default;
 
-  void command(std::string command) noexcept { command_ = command; }
-  void set_parameter(std::string name, std::string value) { params_[name] = value; }
-  void remove_parameter(std::string name) {
+  void command(const std::string& command) noexcept { command_ = command; }
+  void set_parameter(const std::string& name, const std::string& value) { params_[name] = value; }
+  void remove_parameter(const std::string& name) {
     if (params_.count(name)) {
       params_.erase(name);
     }
   }
 
   std::string command() const noexcept { return command_; }
-  std::unordered_map<std::string, std::string> paramteres() noexcept { return params_; }
+  std::unordered_map<std::string, std::string> parameters() noexcept { return params_; }
 
 private:
-  std::string command_ = "";
+  std::string command_;
   std::unordered_map<std::string, std::string> params_;
 };
 
